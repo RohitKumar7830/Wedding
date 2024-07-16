a0cfae45e101a2622cb5ca474c9c0bb82ad021a8
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-16 15:02:55 +0530

    app logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export const Logo = () => {
+  return (
+    <div className=" text-2xl tracking-tighter font-bold">
+      <Link to={'/'}>
+        MANE
+      </Link>
+    </div>
+  )
+}
