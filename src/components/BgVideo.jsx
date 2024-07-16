b41dfda8c6871c1c402fb75f1a9cc3853d7c3a4a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-16 23:27:49 +0530

    adding down animation

--- src/components/BgVideo.jsx
+++ src/components/BgVideo.jsx
@@ -1,8 +1,10 @@
 import videoBg from '../assets/backgroundVideo.mp4';
+import { IoIosArrowDown } from "react-icons/io";
 
 export const BgVideo = () => {
   return (
     <div className="relative w-screen overflow-hidden" style={{ height: 'calc(100vh - 70px)' }}>
+
       <video
         src={videoBg}
         autoPlay
@@ -10,12 +12,20 @@ export const BgVideo = () => {
         muted
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
-      <div className="flex flex-col justify-center items-center relative top-[25%] text-white gap-y-8">
+
+      <div className="flex flex-col justify-center items-center relative top-[30%] text-white gap-y-8">
         <div className=" font-thin tracking-widest">LET US MAKE</div>
         <div className=" text-6xl  flex tracking-wide items-center justify-center text-center font-serif">
           The Wedding Of Your Dreams
         </div>
       </div>
+
+      <div className="flex flex-col justify-center items-center relative top-[75%] text-white gap-y-8">
+        <div className=' bg-white h-14 w-7 rounded-3xl flex items-center justify-center'>
+          <IoIosArrowDown className=' text-slate-500 size-6 animate-bounce' />
+        </div>
+      </div>
+
     </div>
   );
 };
