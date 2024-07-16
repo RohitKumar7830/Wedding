5d0e69737bc0bf5729840648bfa7c3946f336fdf
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-16 09:26:09 +0530

    create pages

---
+++ src/pages/Stories.jsx
@@ -0,0 +1,7 @@
+export const Stories = () => {
+  return (
+    <div>
+      Stories
+    </div>
+  )
+}
---
+++ src/pages/pages.js
@@ -0,0 +1,7 @@
+import { About } from './About';
+import { Contact } from './Contact';
+import { Home } from './Home';
+import { Services } from './Services';
+import { Stories } from './Stories';
+
+export { About, Contact, Home, Services, Stories };
