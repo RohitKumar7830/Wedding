774f673ae6f415253a4f9e491272beb2c0a9c3e5
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-18 00:14:32 +0530

    add cards

--- src/components/BgVideo.jsx
+++ src/components/BgVideo.jsx
@@ -3,8 +3,7 @@ import { IoIosArrowDown } from "react-icons/io";
 
 export const BgVideo = () => {
   return (
-    <div className="relative w-screen overflow-hidden" style={{ height: 'calc(100vh - 70px)' }}>
-
+    <div className="relative w-screen h-screen overflow-hidden">
       <video
         src={videoBg}
         autoPlay
@@ -12,20 +11,17 @@ export const BgVideo = () => {
         muted
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
-
       <div className="flex flex-col justify-center items-center relative top-[30%] text-white gap-y-8">
-        <div className=" font-thin tracking-widest">LET US MAKE</div>
-        <div className=" text-6xl  flex tracking-wide items-center justify-center text-center font-serif">
+        <div className="font-thin tracking-widest">LET US MAKE</div>
+        <div className="text-6xl flex tracking-wide items-center justify-center text-center font-serif">
           The Wedding Of Your Dreams
         </div>
       </div>
-
-      <div className="flex flex-col justify-center items-center relative top-[65%] md:top-[75%] text-white gap-y-8">
-        <div className=' bg-white h-12 w-7 rounded-3xl flex items-center justify-center'>
-          <IoIosArrowDown className=' text-slate-500 size-6 animate-bounce' />
+      <div className="flex flex-col justify-center items-center absolute bottom-16 text-white gap-y-8">
+        <div className='bg-white h-12 w-7 rounded-3xl flex items-center justify-center'>
+          <IoIosArrowDown className='text-slate-500 size-6 animate-bounce' />
         </div>
       </div>
-
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Featured } from '../components/Featured';
 
 export const Home = () => {
   return (
-    <div className=' overflow-x-hidden flex flex-col justify-center items-center'>
+    <div className='overflow-x-hidden flex flex-col justify-center items-center min-h-screen'>
       <BgVideo />
       <Featured />
     </div>
