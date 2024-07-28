acc8fd6f4b7966bf8788806f14eb3340b76d8ea3
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-28 22:09:03 +0530

    init services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home, Contact, Services, Stories, About } from './pages/pages.js';
-import { Navbar } from './components/Navbar.jsx';
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,3 +1,16 @@
+import { Navbar } from '../components/Navbar';
+
 export const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div>
+      <div className='h-[70px]'>
+        <Navbar />
+      </div>
+      <div className='bg-sky-300 h-screen w-full'>
+        <div>
+          Settings
+        </div>
+      </div>
+    </div>
+  )
 };
