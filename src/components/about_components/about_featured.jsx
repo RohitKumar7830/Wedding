756ef3d863c66dc09062f73106cb04611cb659e4
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-10 02:06:48 +0530

    init teams, stories

--- src/components/about_components/about_featured.jsx
+++ src/components/about_components/about_featured.jsx
@@ -1,9 +1,19 @@
 import { NewspaperSection } from '../NewsPaperSection'
+import { Stories } from '../stories'
+import { AboutText } from './aboutText'
+import { Team } from './team'
 
 export const AboutFeatured = () => {
   return (
     <div className="container flex flex-col max-w-7xl w-full py-14">
       <NewspaperSection />
+
+      <AboutText />
+
+      {/* <Team />
+
+      <Stories /> */}
+
     </div>
   )
 }
